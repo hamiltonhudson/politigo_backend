e7d9c9550b6a0d472444a885b5985f2417385f66
Hamilton Hudson <ehamiltonhudson@gmail.com>
2019-03-25 01:01:44 -0400

    adds users to event serializer for more dynamic display of all activists info

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,4 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :cause, :style, :location, :lat, :long, :date, :time
+  attributes :id, :cause, :style, :location, :lat, :long, :date, :time, :users
   has_many :users, serializer: UserSerializer
 end
